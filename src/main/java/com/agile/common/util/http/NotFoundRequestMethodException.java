634f1ad39e213049ecf026b1364668a71b71c24b
tongmeng@idss-cn.com <tongmeng@idss-cn.com>
2020-06-05 18:38:39 +0800

    feat(补充HttpUtil工具):
    新增http、https解析，post\get\put\delete方式支持，自动解析header、param参数等功能

---
+++ src/main/java/com/agile/common/util/http/NotFoundRequestMethodException.java
@@ -0,0 +1,11 @@
+package com.agile.common.util.http;
+
+/**
+ * @author 佟盟
+ * 日期 2020/6/5 15:28
+ * 描述 找不到对应的请求方法
+ * @version 1.0
+ * @since 1.0
+ */
+public class NotFoundRequestMethodException extends Exception {
+}

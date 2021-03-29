6868607e33e3cad7a359e9bd1f38b6dc7470a946
TongMeng <mydeathtrial@163.com>
2021-03-29 20:14:31 +0800

    fix(修复数字类型数据排序):

--- src/main/java/cloud/agileframework/common/util/collection/CollectionsUtil.java
+++ src/main/java/cloud/agileframework/common/util/collection/CollectionsUtil.java
@@ -1,6 +1,8 @@
 package cloud.agileframework.common.util.collection;
 
+import cloud.agileframework.common.util.number.NumberUtil;
 import cloud.agileframework.common.util.object.ObjectUtil;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -31,21 +33,49 @@ public class CollectionsUtil {
                         break;
                     }
                 }
-            } catch (IllegalAccessException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return v;
         });
     }
 
-    private static <T> int compare(T o1, T o2, String property, boolean sort) throws IllegalAccessException {
-        int result = 0;
-        if (Map.class.isAssignableFrom(o1.getClass())) {
-            result = String.valueOf(((Map) o1).get(property)).compareTo(String.valueOf(((Map) o2).get(property)));
-        } else {
-            result = String.valueOf(ObjectUtil.getFieldValue(o1, property))
-                    .compareTo(String.valueOf(ObjectUtil.getFieldValue(o2, property)));
-        }
+    private static <T> int compare(T o1, T o2, String property, boolean sort) {
+        Object o1Value = getValue(o1, property);
+        Object o2Value = getValue(o2, property);
+        int result = compare(o1Value, o2Value);
         return sort ? result : -result;
+    }
+
+    private static Object getValue(Object o1, String property) {
+        if (o1 == null) {
+            return null;
+        }
+        if (Map.class.isAssignableFrom(o1.getClass())) {
+            return ((Map) o1).get(property);
+        } else {
+            return ObjectUtil.getFieldValue(o1, property);
+        }
+    }
+
+    private static int compare(Object o1, Object o2) {
+        if (o1 == null && o2 == null) {
+            return 0;
+        } else if (o1 == null) {
+            return -1;
+        } else if (o2 == null) {
+            return 1;
+        }
+
+        if (o1 == o2) {
+            return 0;
+        }
+
+        if (NumberUtil.isNumber(o1.getClass()) && NumberUtil.isNumber(o2.getClass())) {
+            return NumberUtils.createDouble(o1.toString())
+                    .compareTo(NumberUtils.createDouble(o2.toString()));
+        } else {
+            return o1.toString().compareTo(o2.toString());
+        }
     }
 }

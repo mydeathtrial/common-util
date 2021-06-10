5b853aa197be911143830af79f192e8c84151977
TongMeng <mydeathtrial@163.com>
2021-06-10 14:44:08 +0800

    fix(DateUtil): 时间戳解析

--- src/main/java/com/agile/common/util/date/DateUtil.java
+++ src/main/java/com/agile/common/util/date/DateUtil.java
@@ -1,9 +1,14 @@
 package com.agile.common.util.date;
 
-import com.agile.common.util.object.ObjectUtil;
+import com.agile.common.constant.Constant;
 import com.agile.common.util.pattern.PatternUtil;
+import org.apache.commons.lang3.ObjectUtils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
@@ -29,7 +34,7 @@ public class DateUtil {
     /**
      * 补零时间正则
      */
-    private static final String ZERO_FILL_TIME_REGEX = "(?<hour>(0[0-9]|1[0-9]|2[0-3]))(((:|时|点|-|/)(?<minute>[0-5][0-9])(((:|分|-|/)(?<second>[0-5][0-9]))?))|((:|时|点)))";
+    private static final String ZERO_FILL_TIME_REGEX = "(?<hour>(0[0-9]|1[0-9]|2[0-4]|[0-9]))(((:|时|点|-|/)(?<minute>([0-5][0-9]|[0-9]))(((:|分|-|/)(?<second>([0-5][0-9]|[0-9])))?))|((:|时|点)))";
     /**
      * 时间正则
      */
@@ -37,12 +42,12 @@ public class DateUtil {
     /**
      * 简单日期正则
      */
-    private static final String DATE_SIMPLE_REGEX = "(?<year0>([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3}))(?<split1>[年|/|-]+)(((?<month0>(1[012]|0?[1-9])))?)";
+    private static final String DATE_SIMPLE_REGEX = "(?<year0>([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3}))(?<split1>[年|/|-| ]+)(((?<month0>(1[012]|0?[1-9])))?)(?<split2>[月|/|-| ]+)(((?<date0>([12][0-9]|3[01]|0?[1-9])))?)";
 
     /**
-     * 时间戳正则
+     * 毫秒时间戳正则
      */
-    private static final String TIME_MILLIS_FORMAT = "1[\\d]{12}";
+    private static final String TIME_MILLIS_FORMAT = "-?[\\d]*";
 
     /**
      * 下午标识
@@ -73,13 +78,15 @@ public class DateUtil {
                 gregorianCalendar = parseDate(source);
 
                 if (gregorianCalendar == null) {
-                    return parseTime(source);
+                    GregorianCalendar time = parseTime(source);
+                    parsePM(time, source);
+                    return time;
                 } else {
                     List<String> list = PatternUtil.getMatched(ZERO_FILL_DATE_REGEX, source);
-                    if (ObjectUtil.isEmpty(list)) {
+                    if (ObjectUtils.isEmpty(list)) {
                         list = PatternUtil.getMatched(DATE_REGEX, source);
                     }
-                    if (ObjectUtil.isEmpty(list)) {
+                    if (ObjectUtils.isEmpty(list)) {
                         list = PatternUtil.getMatched(DATE_SIMPLE_REGEX, source);
                     }
                     for (String node : list) {
@@ -95,11 +102,13 @@ public class DateUtil {
                                 time.get(Calendar.MINUTE),
                                 time.get(Calendar.SECOND));
                     }
+
                 }
             }
         } else if (step.length > 1) {
 
             gregorianCalendar = new GregorianCalendar();
+            gregorianCalendar.set(Calendar.MILLISECOND, 0);
 
             boolean haveDate = false;
             for (String s : step) {
@@ -121,7 +130,7 @@ public class DateUtil {
         } else {
             return null;
         }
-
+        parsePM(gregorianCalendar, source);
         return gregorianCalendar;
     }
 
@@ -160,6 +169,22 @@ public class DateUtil {
     }
 
     /**
+     * 分析上下午
+     *
+     * @param gregorianCalendar 时间
+     * @param source            源字符串
+     */
+    private static void parsePM(GregorianCalendar gregorianCalendar, String source) {
+        if (gregorianCalendar == null) {
+            return;
+        }
+        int hourOfDay = gregorianCalendar.get(Calendar.HOUR_OF_DAY);
+        if (hourOfDay <= Constant.NumberAbout.TWELVE && PatternUtil.find(PM_FORMAT, source)) {
+            gregorianCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay + 12);
+        }
+    }
+
+    /**
      * 从字符串中提取日期
      *
      * @param source 包含日期的字符串
@@ -177,6 +202,10 @@ public class DateUtil {
             GregorianCalendar gregorianCalendar = new GregorianCalendar();
             gregorianCalendar.set(Calendar.MONTH, 0);
             gregorianCalendar.set(Calendar.DATE, 1);
+            gregorianCalendar.set(Calendar.HOUR_OF_DAY, 0);
+            gregorianCalendar.set(Calendar.MINUTE, 0);
+            gregorianCalendar.set(Calendar.SECOND, 0);
+            gregorianCalendar.set(Calendar.MILLISECOND, 0);
             Stream<Map.Entry<String, String>> stream = list.entrySet().stream().filter(e -> e.getValue() != null);
 
             stream.forEach(e -> {
@@ -193,10 +222,91 @@ public class DateUtil {
         return null;
     }
 
-//    public static void main(String[] args) {
-//        GregorianCalendar gregorianCalendar = parse("2020-04-23 09:38:00");
-//        String s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(gregorianCalendar.getTime());
-//        System.out.println(s);
-//
-//    }
+    private static final int DATE_UNIT = 60;
+    private static final int MIN_UNIT = 1000;
+    private static final int HOUR_UNIT = 24;
+
+    /**
+     * 获取时间戳字符串
+     */
+    public static Date getCurrentDate() {
+        return new Date(System.currentTimeMillis());
+    }
+
+    /**
+     * 字符串转日期
+     *
+     * @param date   日期字符串
+     * @param format 格式
+     */
+    public static Date toDateByFormat(String date, String format) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.parse(date);
+    }
+
+    public static String toFormatByDate(Date date, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
+
+    /**
+     * 比较两个Date类型时间是否相同
+     *
+     * @param date1 时间1
+     * @param date2 时间2
+     * @return true:相同
+     */
+    public static boolean isSame(Date date1, Date date2) {
+        return date1.getTime() == date2.getTime();
+    }
+
+    /**
+     * 计算两个日期之间相差的天数
+     *
+     * @param date1 时间1
+     * @param date2 时间2
+     * @return 相差天数
+     */
+    public static long getInterval(Date date1, Date date2, int unit) {
+        long interval;
+        switch (unit) {
+            case Calendar.YEAR:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / (MIN_UNIT * DATE_UNIT * DATE_UNIT * HOUR_UNIT * 365);
+                break;
+            case Calendar.MONTH:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / (MIN_UNIT * DATE_UNIT * DATE_UNIT * HOUR_UNIT * 30);
+                break;
+            case Calendar.DATE:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / (MIN_UNIT * DATE_UNIT * DATE_UNIT * HOUR_UNIT);
+                break;
+            case Calendar.HOUR_OF_DAY:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / (MIN_UNIT * DATE_UNIT * DATE_UNIT);
+                break;
+            case Calendar.MINUTE:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / (MIN_UNIT * DATE_UNIT);
+                break;
+            case Calendar.SECOND:
+                interval = Math.abs(date1.getTime() - date2.getTime()) / MIN_UNIT;
+                break;
+            case Calendar.MILLISECOND:
+                interval = Math.abs(date1.getTime() - date2.getTime());
+                break;
+            default:
+                throw new RuntimeException("Undefined variable " + unit);
+        }
+        return interval;
+    }
+
+    /**
+     * 获取指定日期后时间（指定加算年月日信息）
+     *
+     * @param date     指定日期
+     * @param duration 时间间隔
+     * @return 加算后日期
+     */
+    public static Date add(Date date, Duration duration) {
+        Calendar cal = GregorianCalendar.getInstance();
+        cal.setTimeInMillis(date.getTime() + duration.toMillis());
+        return cal.getTime();
+    }
 }

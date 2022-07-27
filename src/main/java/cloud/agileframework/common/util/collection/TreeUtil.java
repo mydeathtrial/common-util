7589e14b98587ec8caf5051f0196205b6d609ad2
佟盟 <tongmeng@superred.com.cn>
2022-07-27 11:04:47 +0800

    build(版本统一): 2.1.0.M34

--- src/main/java/cloud/agileframework/common/util/collection/TreeUtil.java
+++ src/main/java/cloud/agileframework/common/util/collection/TreeUtil.java
@@ -4,6 +4,9 @@ import cloud.agileframework.common.constant.Constant;
 import cloud.agileframework.common.util.clazz.ClassUtil;
 import cloud.agileframework.common.util.object.ObjectUtil;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.Field;
@@ -27,7 +30,7 @@ public class TreeUtil {
      * @param nodes 构建源数据
      * @return 树形结构数据集
      */
-    public static <A extends Serializable, T extends TreeBase<A, T>, B extends T> SortedSet<B> createTree(Collection<B> nodes, A rootValue, String splitChar, Set<Field> fullFieldSet) {
+    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTree(Collection<? extends T> nodes, A rootValue, String splitChar, Set<Field> fullFieldSet) {
         Map<Optional<A>, List<T>> children = nodes.parallelStream().filter(node -> {
             node.getChildren().clear();
             return !Objects.equals(node.getParentId(), rootValue);
@@ -62,7 +65,7 @@ public class TreeUtil {
             fullFieldSet.forEach(b -> {
                 wrapperList.parallelStream().forEach(a -> {
                     Object v = a.getFull(b, splitChar);
-                    if(v == null){
+                    if (v == null) {
                         return;
                     }
                     cache.put(v, (T) a.getCurrent());
@@ -74,7 +77,7 @@ public class TreeUtil {
         return nodes.parallelStream().filter(node -> Objects.equals(node.getParentId(), rootValue)).collect(Collectors.toCollection(ConcurrentSkipListSet::new));
     }
 
-    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTree(Collection<T> list, A rootValue, String splitChar, String... fullFields) {
+    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTree(Collection<? extends T> list, A rootValue, String splitChar, String... fullFields) {
         if (!list.isEmpty()) {
             T entity = list.stream().findAny().get();
             Class<T> tClass = (Class<T>) entity.getClass();
@@ -90,7 +93,78 @@ public class TreeUtil {
         return new ConcurrentSkipListSet<>();
     }
 
-    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTree(Collection<T> list, A rootValue) {
+    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTree(Collection<? extends T> list, A rootValue) {
         return createTree(list, rootValue, Constant.RegularAbout.SPOT);
+    }
+
+    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTreeByNodeIds(Collection<? extends T> all, Collection<A> ids, A rootValue) {
+        return createTreeByNodeIds(all, ids, rootValue, Constant.RegularAbout.DIC_SPLIT);
+    }
+
+    /**
+     * 创建跟主键相关的树形结构，一般用于数据权限的使用场景
+     *
+     * @param all        完整树的全部数据
+     * @param ids        想要查询相关节点的主键集合
+     * @param rootValue  跟节点值
+     * @param splitChar  分隔符
+     * @param fullFields 累加字段
+     * @param <A>        主键类型
+     * @param <T>        树元素类型
+     * @return 树
+     */
+    public static <A extends Serializable, T extends TreeBase<A, T>> SortedSet<T> createTreeByNodeIds(Collection<? extends T> all, Collection<A> ids, A rootValue, String splitChar, String... fullFields) {
+        if (CollectionUtils.isEmpty(ids)) {
+            all.clear();
+            return Sets.newTreeSet();
+        } else {
+            List<TreeBaseProxy<A, T>> allWrapper = all.stream().map(TreeBaseProxy::new).collect(Collectors.toList());
+            createTree(allWrapper, rootValue, splitChar, "fullId");
+
+            Set<String> relevantNode = allWrapper
+                    .stream()
+                    .filter(m -> ids.contains(m.getId())).flatMap(m -> Arrays.stream(StringUtils.split(m.getFullId(), splitChar))).collect(Collectors.toSet());
+            allWrapper.removeIf(m -> !relevantNode.contains(String.valueOf(m.getId())));
+            List<T> allNodes = allWrapper.stream().map(TreeBaseProxy::getTreeBase).map(a -> (T) a).collect(Collectors.toList());
+            return createTree(allNodes, rootValue, splitChar, fullFields);
+        }
+    }
+
+    public static class TreeBaseProxy<A extends Serializable, T extends TreeBase<A, T>> extends TreeBase<A, TreeBaseProxy<A, T>> {
+        private final T treeBase;
+
+        public TreeBaseProxy(T treeBase) {
+            this.treeBase = treeBase;
+            this.fullId = String.valueOf(getId());
+        }
+
+        @Override
+        public A getId() {
+            return treeBase.getId();
+        }
+
+        @Override
+        public A getParentId() {
+            return treeBase.getParentId();
+        }
+
+        @Override
+        public Integer getSort() {
+            return treeBase.getSort();
+        }
+
+        private String fullId;
+
+        public String getFullId() {
+            return fullId;
+        }
+
+        public void setFullId(String fullId) {
+            this.fullId = fullId;
+        }
+
+        public TreeBase<A, T> getTreeBase() {
+            return treeBase;
+        }
     }
 }
